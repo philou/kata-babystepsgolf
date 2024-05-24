80cbe763900b52ab80fcce0150bfca70601c047f
Philippe Bourgau <philippe.bourgau@gmail.com>
2024-05-24 16:21:22 +0200

    Add initial solution for Hole1: Pascal Triangle
    To have something to start from
    - Import solution and tests from Rabah in Murex XP-Coaching repo
    - Rename PascalTriangle to Hole1PascalTriangle

---
+++ java/src/main/java/com/murex/Hole1PascalTriangle.java
@@ -0,0 +1,66 @@
+/*
+Copyright (c) 2023 Murex
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package com.murex;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Hole1PascalTriangle {
+
+    public static String draw(int lastLine) {
+        List<List<Integer>> matrix = computeMatrix(lastLine);
+        return convertMatrix(matrix);
+    }
+
+    static String convertLine(List<Integer> integers) {
+        return integers.stream().map(String::valueOf)
+                .collect(Collectors.joining(" "));
+    }
+
+    static String convertMatrix(List<List<Integer>> matrix){
+        return matrix.stream()
+                .map(innerLine -> convertLine(innerLine) + "\n")
+                .collect(Collectors.joining(""));
+
+    }
+    static List<List<Integer>> computeMatrix(int size) {
+        List<List<Integer>> matrix = new ArrayList<>();
+        matrix.add(List.of(1));
+        for(int i = 0; i < size; i++){
+            matrix.add(computeNext(matrix.get(i)));
+        }
+        return matrix;
+    }
+    static List<Integer> computeNext(List<Integer> currentLine) {
+        var result = new ArrayList<Integer>();
+        result.add(1);
+
+        for (int i = 0; i < currentLine.size() - 1; i++) {
+            result.add(currentLine.get(i) + currentLine.get(i + 1));
+        }
+
+        result.add(1);
+        return result;
+    }
+}
---
+++ java/src/test/java/com/murex/Hole1PascalTriangleTest.java
@@ -0,0 +1,147 @@
+/*
+Copyright (c) 2023 Murex
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package com.murex;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class Hole1PascalTriangleTest {
+
+    @Test
+    public void acceptance_test() {
+        int lastLine = 7;
+        String expected = """
+                1
+                1 1
+                1 2 1
+                1 3 3 1
+                1 4 6 4 1
+                1 5 10 10 5 1
+                1 6 15 20 15 6 1
+                1 7 21 35 35 21 7 1
+                """;
+        assertEquals(expected, Hole1PascalTriangle.draw(lastLine));
+    }
+
+    @Test
+    public void triangle_lastline_0(){
+        String expected = """
+                1
+                """;
+        assertEquals(expected, Hole1PascalTriangle.draw(0));
+    }
+
+    @Test
+    public void triangle_lastline_1(){
+        String expected = """
+                1
+                1 1
+                """;
+        assertEquals(expected, Hole1PascalTriangle.draw(1));
+    }
+
+    @Test
+    public void triangle_lastline_2(){
+        String expected = """
+                1
+                1 1
+                1 2 1
+                """;
+        assertEquals(expected, Hole1PascalTriangle.draw(2));
+    }
+
+    @Test
+    void triangle_lastline_3() {
+        String expected = """
+            1
+            1 1
+            1 2 1
+            1 3 3 1
+            """;
+        assertEquals(expected, Hole1PascalTriangle.draw(3));
+    }
+
+    @Test
+    public void compute_next_line_for_1(){
+        List<Integer> expected = List.of(1, 1);
+        assertEquals(expected, Hole1PascalTriangle.computeNext(List.of(1)));
+    }
+
+    @Test
+    public void compute_next_line_for_1_1(){
+        List<Integer> expected = List.of(1, 2, 1);
+        assertEquals(expected, Hole1PascalTriangle.computeNext(List.of(1, 1)));
+    }
+    @Test
+    public void compute_next_line_for_1_3_3_1(){
+        List<Integer> expected = List.of(1, 4, 6, 4, 1);
+        assertEquals(expected, Hole1PascalTriangle.computeNext(List.of(1,3, 3, 1)));
+    }
+
+    @Test
+    public void compute_next_line_for_1_2_1(){
+        List<Integer> expected = List.of(1, 3, 3, 1);
+        assertEquals(expected, Hole1PascalTriangle.computeNext(List.of(1, 2, 1)));
+    }
+
+    @Test
+    public void convert_line_to_string_representation_1_1() {
+        assertEquals("1 1", Hole1PascalTriangle.convertLine(List.of(1,1)));
+    }
+
+    @Test
+    void convert_matrix_of_size_1() {
+        var matrix = List.of(List.of(1));
+        assertEquals("1\n", Hole1PascalTriangle.convertMatrix(matrix));
+    }
+
+    @Test
+    void convert_matrix_of_size_2() {
+        var matrix = List.of(
+                List.of(1),
+                List.of(1, 1)
+        );
+        String expected = """
+                1
+                1 1
+                """;
+        assertEquals(expected, Hole1PascalTriangle.convertMatrix(matrix));
+    }
+
+    @Test
+    void convert_matrix_of_size_3() {
+        var matrix = List.of(
+                List.of(1),
+                List.of(1, 1),
+                List.of(1, 2, 1)
+        );
+        String expected = """
+                1
+                1 1
+                1 2 1
+                """;
+        assertEquals(expected, Hole1PascalTriangle.convertMatrix(matrix));
+    }
+}
