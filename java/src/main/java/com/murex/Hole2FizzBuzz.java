195bee56ade50c7235aeeb3b35836ff933a45401
Philippe Bourgau <philippe.bourgau@gmail.com>
2024-05-27 15:29:42 +0200

    ✅ TCR - tests passing
    changed-lines:
        src: 6
        test: 0
    test-stats:
        run: 23
        passed: 23
        failed: 0
        skipped: 4
        error: 0
        duration: 46ms

--- java/src/main/java/com/murex/Hole2FizzBuzz.java
+++ java/src/main/java/com/murex/Hole2FizzBuzz.java
@@ -26,11 +26,11 @@ public class Hole2FizzBuzz {
     public static String fizzBuzz(int number) {
         String result = "";
         if (number % 15 == 0) {
-            result = "FizzBuzz";
+            result += "FizzBuzz";
         } else if (number % 3 == 0) {
-            result = "Fizz";
+            result += "Fizz";
         } else if (number % 5 == 0) {
-            result = "Buzz";
+            result += "Buzz";
         }
         if (result.isBlank()) {
             result = Integer.toString(number);
