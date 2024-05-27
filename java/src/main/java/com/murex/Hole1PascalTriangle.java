35d70186b568303c79f1f58a822e93d10f0fb9b6
Philippe Bourgau <philippe.bourgau@gmail.com>
2024-05-27 11:23:15 +0200

    ✅ TCR - tests passing
    changed-lines:
        src: 4
        test: 0
    test-stats:
        run: 14
        passed: 14
        failed: 0
        skipped: 4
        error: 0
        duration: 29ms

--- java/src/main/java/com/murex/Hole1PascalTriangle.java
+++ java/src/main/java/com/murex/Hole1PascalTriangle.java
@@ -72,12 +72,12 @@ class Hole1PascalTriangle {
     private static String arrangeLinesIsoceles(List<String> stringLines) {
         int width = stringLines.get(stringLines.size() - 1).length();
         return stringLines.stream()
-                .map(line -> justify(line))
+                .map(line -> justify(line, width))
                 .map(line -> line + "\n")
                 .collect(Collectors.joining(""));
     }
 
-    private static String justify(String line) {
+    private static String justify(String line, int width) {
         return line;
     }
 
