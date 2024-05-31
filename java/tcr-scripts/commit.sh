dea5dea17d3d435bca173ee19ead4229b7e550bb
Philippe Bourgau <philippe.bourgau@gmail.com>
2024-05-31 12:10:47 +0200

    Update commit.sh to take 3 arguments (commit type, baby step name, details)
    To force even more consciousness about the baby steps taken
    - validate arguments
    - add refs to online resources with the standards
    - format the commit message like "✅ commit_type baby_step_type details"

--- java/tcr-scripts/commit.sh
+++ java/tcr-scripts/commit.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env sh
 
 # Check that a commit message is provided
-if [ "$#" -ne 1 ]; then
-  echo "Error: Missing a commit message argument"
+if [ "$#" -ne 3 ]; then
+  echo "Error: usage: ./tcr.sh <commit type> <baby step type> <details>"
+  echo "  - commit type: 'refactor' or 'feat' (cf https://www.conventionalcommits.org/en/v1.0.0/)"
+  echo "  - baby step type:"
+  echo "    - refactorings: https://refactoring.com/catalog/"
+  echo "    - features: https://en.wikipedia.org/wiki/Transformation_Priority_Premise"
+  echo "  - details: extra info about the change"
   exit 1
 fi
 
-# Store the argument as the commit message
-commit_message="✔ $1"
+# Store the arguments in variables
+commit_type="$1"
+baby_steps_type="$2"
+commit_details="$3"
+
+# Validate the commit_type
+if [ "$commit_type" != "feat" ] && [ "$commit_type" != "refactor" ]; then
+  echo "Error: The commit type must be either 'feat' or 'refactor' (cf https://www.conventionalcommits.org/en/v1.0.0/)"
+  exit 1
+fi
 
 # Get the current working branch from git
 current_branch=$(git branch --show-current)
@@ -18,6 +31,12 @@ if [ -z "$current_branch" ]; then
   exit 1
 fi
 
+# Add all changes to the staging area
 git add .
+
+# Commit with the provided commit message
+commit_message="✅ $commit_type: $baby_steps_type: $commit_details"
 git commit --message "$commit_message"
+
+# Push the commit to the origin
 git push --no-recurse-submodules origin "$current_branch"
