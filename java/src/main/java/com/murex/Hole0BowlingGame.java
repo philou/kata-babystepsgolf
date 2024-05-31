3dd3cd473e2b8b90074ae7867c00908fbd5257c6
Philippe Bourgau <philippe.bourgau@gmail.com>
2024-05-31 13:04:02 +0200

    ✅ refactor: extract method: spareBonus

--- java/src/main/java/com/murex/Hole0BowlingGame.java
+++ java/src/main/java/com/murex/Hole0BowlingGame.java
@@ -13,7 +13,7 @@ public class Hole0BowlingGame {
                 score += TOTAL_PINS + strikeBonus(rolls, iRoll);
                 iRoll++;
             } else if (isSpare(rolls, iRoll)) {
-                score += TOTAL_PINS + rolls[iRoll + 2];
+                score += TOTAL_PINS + spareBonus(rolls, iRoll);
                 iRoll += 2;
             } else {
                 score += rolls[iRoll] + rolls[iRoll + 1];
@@ -21,6 +21,10 @@ public class Hole0BowlingGame {
             }
         }
         return score;
+    }
+
+    private static int spareBonus(int[] rolls, int iRoll) {
+        return rolls[iRoll + 2];
     }
 
     private static boolean isSpare(int[] rolls, int iRoll) {
